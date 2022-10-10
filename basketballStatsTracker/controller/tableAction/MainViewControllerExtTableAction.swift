cc690c2b0034463466eb9c730b765989ad6b1a17
fanzhiyong10 <13700318430@163.com>
2022-10-10 15:16:11 +0800

    Set Defs

--- basketballStatsTracker/controller/tableAction/MainViewControllerExtTableAction.swift
+++ basketballStatsTracker/controller/tableAction/MainViewControllerExtTableAction.swift
@@ -346,7 +346,7 @@ extension MainViewController {
         
         print("\(myLabel.indexPath!.row)")
         
-        let vc = SetPlayersViewController()
+        let vc = SetDefsViewController()
         vc.liveData = self.allLiveDatas[myLabel.indexPath!.row]
         vc.indexPath = myLabel.indexPath
         vc.delegate = self
